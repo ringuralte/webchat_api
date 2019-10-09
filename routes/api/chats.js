c84a28f6873d6cb1004d6a224f298d2fa973ac98
ringuralte <ringuralte@gmail.com>
2019-10-09 11:45:02 +0530

    chats

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -19,4 +19,22 @@ router.get("/api/getChats", (req, res) => {
     });
 });
 
+//? maybe try another query that selects everything from the insertId and return that in response
+router.post("/api/postChat", (req, res) => {
+  const { sender, msg } = req.body;
+  const queryString =
+    "INSERT into chats (sender, msg, time_send, titleid, userid) values (?, ?, NOW(), (select id from topics where id=4), (select id from users where user_id=?))";
+  db.query(queryString, [sender, msg, sender])
+    .then(result => {
+      console.log(result.insertId);
+      console.log(result.affectedRows);
+      res.status(200).json({
+        id: result.insertId
+      });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
 module.exports = router;
