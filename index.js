3857eb9b4e2ce74c046b139a5892e128a798bc79
ringuralte <ringuralte@gmail.com>
2019-11-23 22:43:51 +0530

    socketio port test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path")
 
 require("dotenv").config();
 const app = express();
-const socketServer = app.listen(8080);
+const socketServer = app.listen(80);
 const io = require("socket.io").listen(socketServer);
 
 const jwtAuth = require("./routes/authentication");
