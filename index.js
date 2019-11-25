b0dac4841c59bffd18c99d604ae702b7d5f0e5b9
ringuralte <ringuralte@gmail.com>
2019-11-25 10:59:56 +0530

    socketio port test v10

--- index.js
+++ index.js
@@ -11,17 +11,16 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app)
-const io = require("socket.io")(server, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin,
-      "Access-Control-Allow-Credentials": true
-    };
-    res.writeHeader(200, headers);
-    res.end();
-  }
-})
+const options = {
+  allowUpgrades: true,
+  transports: ['polling', 'websocket'],
+  pingTimeout: 9000,
+  pingInterval: 3000,
+  cookie: 'io',
+  httpCompression: true,
+  origins: '*:*'
+}
+const io = require("socket.io")(server, options)
 
 const userRouter = require("./routes/api/users");
 const topicsRoute = require("./routes/api/topics");
@@ -38,13 +37,6 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "client/out")))
 
-// const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-// const http = require('http')
-// const socketServer = http.createServer(app)
-// const io = require("socket.io").listen(socketServer)
-// socketServer.listen(80)
 io.on("connection", socket => {
   console.log("user connected");
   socket.on("chat message", function(msg) {
@@ -52,9 +44,10 @@ io.on("connection", socket => {
     socket.emit("chat message", msg);
   });
 });
-server.listen(3000)
+
 app.use(checkTokenRoute);
 app.use(userRouter);
 app.use(topicsRoute);
 app.use(chatsRoute);
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+server.listen(3000, () => console.log(`Socketio Listening on 3000`))
