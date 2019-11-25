7adfe0730b0e7726709fb07455f3e998d9a392b7
ringuralte <ringuralte@gmail.com>
2019-11-25 12:13:08 +0530

    socketio port test v14

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const topicsRoute = require("./routes/api/topics");
 const chatsRoute = require("./routes/api/chats");
 const checkTokenRoute = require("./routes/api/checkToken");
 
+//middleware
 app.use(
   cors({
     origin: "https://fast-oasis-98847.herokuapp.com:3000",
@@ -36,13 +37,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//serving file
 app.use(express.static(path.join(__dirname, "client/out")));
+
+//custom routes
 app.use(checkTokenRoute);
 app.use(userRouter);
 app.use(topicsRoute);
 app.use(chatsRoute);
+
+//listen
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 server.listen(3000, () => console.log(`Socketio Listening on 3000`));
+
+//socketio
 io.on("connection", socket => {
   console.log("user connected");
   socket.on("chat message", function(msg) {
