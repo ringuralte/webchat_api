ed094d712f00336f8e16ad0135b330ba3f5626db
ringuralte <ringuralte@gmail.com>
2019-11-25 12:02:00 +0530

    socketio port test v13

--- index.js
+++ index.js
@@ -9,33 +9,40 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-const server = require('http').Server(app)
-const options = {
-  allowUpgrades: true,
-  transports: ['polling', 'websocket'],
-  pingTimeout: 9000,
-  pingInterval: 3000,
-  cookie: 'io',
-  httpCompression: true,
-  origins: '*:*'
-}
-const io = require("socket.io")(server, options)
+const server = require("http").Server(app);
+// const options = {
+//   allowUpgrades: true,
+//   transports: ['polling', 'websocket'],
+//   pingTimeout: 9000,
+//   pingInterval: 3000,
+//   cookie: 'io',
+//   httpCompression: true,
+//   origins: '*:*'
+// }
+const io = require("socket.io")(server);
 
 const userRouter = require("./routes/api/users");
 const topicsRoute = require("./routes/api/topics");
 const chatsRoute = require("./routes/api/chats");
 const checkTokenRoute = require("./routes/api/checkToken");
 
-app.use(cors({
-  origin: "https://fast-oasis-98847.herokuapp.com:3000",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://fast-oasis-98847.herokuapp.com:3000",
+    credentials: true
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "client/out")))
-
+app.use(express.static(path.join(__dirname, "client/out")));
+app.use(checkTokenRoute);
+app.use(userRouter);
+app.use(topicsRoute);
+app.use(chatsRoute);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+server.listen(80, () => console.log(`Socketio Listening on 80`));
 io.on("connection", socket => {
   console.log("user connected");
   socket.on("chat message", function(msg) {
@@ -44,9 +51,3 @@ io.on("connection", socket => {
   });
 });
 
-app.use(checkTokenRoute);
-app.use(userRouter);
-app.use(topicsRoute);
-app.use(chatsRoute);
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-server.listen(3000, () => console.log(`Socketio Listening on 3000`))
